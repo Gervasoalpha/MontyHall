8e4d887162413d1e8d64ff4b01d3823934c07e6d
JACKPOT <JACKPOT@JACOPO-PC>
2023-06-20 00:53:45 +0200

    Creato test per il paradosso di Monty Hall

---
+++ MontyHallLibrary/MontyHallGame.cs
@@ -0,0 +1,130 @@
+﻿namespace MontyHallLibrary
+{
+    // 0 is a goat
+    // 1 is a car
+    public class MontyHallGame
+    {
+        public Random generator = new Random();
+        public List<int>? porte { get; set; }
+        int portaaperta { get; set; }
+        int portascelta { get; set; }
+
+        public MontyHallGame()
+        {
+            var lista = createlist();
+
+            while (!checklist(lista))
+            {
+                lista = createlist();
+            }
+
+            porte = lista;
+
+        }
+        public static void testautomatico(int partite, string scelta)
+        {
+            int vittorie = 0;
+            string scritta = "";
+            int choice;
+
+            if (scelta == "cambiando")
+            {
+                choice = 1;
+                scritta = "cambiando porta";
+            }
+            else
+            {
+                choice = 0;
+                scritta = "non cambiando porta";
+            }
+
+            for (int i = 0; i < partite; i++)
+            {
+
+                MontyHallGame montyHallGame = new MontyHallGame();
+
+                int userdoor = montyHallGame.generator.Next(0, 3);
+                montyHallGame.opendoor(userdoor);
+
+
+                var res = montyHallGame.verificavincita(choice);
+                if (res == "Hai Vinto!")
+                {
+                    vittorie++;
+                }
+
+            }
+            double percentuale = (double)vittorie / (double)partite;
+            Console.WriteLine($"Percentuale di vittoria {scritta}: {percentuale * 100}%");
+        }
+        private List<int> createlist()
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(generator.Next(0, 2));
+            }
+            return list;
+        }
+
+
+        private bool checklist(List<int> list)
+        {
+            var ritorno = list.FindAll(x => x == 1);
+
+            if (ritorno.Count > 1 || ritorno.Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public int opendoor(int choice)
+        {
+            var lista = new List<int>();
+            this.porte.ForEach(x => lista.Add(x));
+            this.portascelta = choice;
+            lista[choice] = 3;
+
+            int caso = generator.Next(0, 3);
+
+            while (!(lista[caso] == 0))
+            {
+
+                caso = generator.Next(0, 3);
+            }
+            // Console.WriteLine($"Dietro la porta {caso + 1} c'è una capra! ");
+            this.portaaperta = caso;
+            return caso;
+        }
+
+        public string verificavincita(int scelta)
+        {
+            if (scelta == 1)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (!(this.portaaperta == i || this.portascelta == i))
+                    {
+                        portascelta = i;
+                        break;
+                    }
+                }
+
+            }
+
+            if (porte[portascelta] == 1)
+            {
+                return "Hai Vinto!";
+            }
+            else
+            {
+                return "Hai Perso!";
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using MontyHallLibrary;
+//while (true)
+//{
+//    MontyHallGame montyHallGame = new MontyHallGame();
+
+
+//    printdoors(montyHallGame.porte);
+
+//    Console.WriteLine("Chose a door: 1,2 or 3");
+//    int userdoor = int.Parse(Console.ReadLine()) - 1;
+//    int portaaperta = montyHallGame.opendoor(userdoor);
+//    printdoors(montyHallGame.porte);
+//    Console.WriteLine("Do you want to change door?");
+//    int choice = int.Parse(Console.ReadLine());
+//    var res = montyHallGame.verificavincita(choice);
+//    Console.WriteLine($"{res}");
+//    Thread.Sleep(5000);
+//}
+Console.WriteLine("VERIFICA DEL PARADOSSO DI MONTY HALL:");
+MontyHallGame.testautomatico(1000000,"cambiando");
+MontyHallGame.testautomatico(1000000, "restando");
+void printdoors<T>(List<T> lista)
+{
+    lista.ForEach(p => Console.WriteLine(p));
+}
